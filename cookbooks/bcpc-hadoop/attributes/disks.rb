c8e805b8475ddb7a23df06f8f3d2780a82c09bb1
Clay Baenziger <cbaenziger@bloomberg.net>
2016-07-12 01:05:30 +0000

    Update disks to work for OpenStack devices and only require one disk for Graphite storage

--- cookbooks/bcpc-hadoop/attributes/disks.rb
+++ cookbooks/bcpc-hadoop/attributes/disks.rb
@@ -1,9 +1,9 @@
 # enumerate available disks and how they will be used
-avail_disks = node[:block_device].keys.select{ |d| d =~ /sd[b-z]/ }
+avail_disks = node[:block_device].keys.select{ |d| d =~ /sd[b-z]|vd[b-z]/ }
 default[:bcpc][:hadoop][:disks] = {
   :available_disks => avail_disks,
   # keep at least that many disks for the :disk_reserve_roles
-  :role_min_disk => 2,
+  :role_min_disk => 1,
   # we are reserving disks for the following
   :reservation_requests => ["graphite_disk"],
   # reservations will be saved here
